dd27c319bda965a95037f33ab3e5981ca12822e5
AyushRamoliya <ayushramoliya69@gmail.com>
2024-07-09 15:39:10 +0530

    Interval-2

--- DOM II/index.js
+++ DOM II/index.js
@@ -1,28 +1,30 @@
-document.querySelector("form").addEventListener("submit",todoFun)
+document.querySelector("form").addEventListener("submit" , Todo)
 
-function todoFun(event){
+function Todo(){
     event.preventDefault()
-    let name = document.getElementById("task").value
-    let id = document.getElementById("priority").value
+    let name = document.getElementById("task").value;
+    let id = document.getElementById("priority").value;
 
     let tr = document.createElement("tr")
     let td1 = document.createElement("td")
+
     td1.innerHTML = name
-    
+
     let td2 = document.createElement("td")
-    td2.innerHTML = id
+
+    td2.innerHTML =  id
+    let td3 = document.createElement("td")
+    td3.innerHTML = "DELETE"
+    tr.append(td1 , td2 , td3)
+
     if(id === "High"){
         td2.style.backgroundColor = "red"
         td2.style.color = "white"
     }
-    if(id === "Low"){
+    else{
         td2.style.backgroundColor = "green"
         td2.style.color = "white"
     }
 
-    let td3 = document.createElement("td")
-    td3.innerHTML = "DELETE"
-    
-    tr.append(td1 , td2 ,td3)
     document.querySelector("tbody").append(tr)
 }
